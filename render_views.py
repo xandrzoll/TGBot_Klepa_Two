f152a36f4ecdd9124d0c6f7f9f2126266e051d1a
Alex Rudkovskiy <xandr.rudkovsky@gmail.com>
2020-02-16 21:51:43 +0300

    Add pattern for renders

--- render_views.py
+++ render_views.py
@@ -3,15 +3,32 @@ from config import *
 from utils.iml_api import iml_check_balance
 
 
-def render_main():
+def render_page(title='', body=''):
     r = '''
-        <h1>Hello this is main page</h1>
-        <p>Choose what you need:</p>
+        <!DOCTYPE html>
+        <html>
+         <head>
+          <meta charset="utf-8">
+          <title>{0}</title>
+         </head>
+         <body>
+          {1}
+         </body>
+        </html>
+    '''
+    return r.format(title, body)
+
+
+def render_main():
+    title = 'Главная страница'
+    body = '''
+        <h1>Добрый день!</h1>
+        <p>Выберите необходимое:</p>
         <ul>
-            <li><a href="/balance_iml">getting the balances from IML API</a></li>
+            <li><a href="/balance_iml">Остатки сим-карт в IML</a></li>
         </ul>
     '''
-    return r
+    return render_page(title, body)
 
 
 def render_balance_iml():
@@ -20,7 +37,7 @@ def render_balance_iml():
     b = {}
 
     try:
-        with open('utils/balance.csv', 'r') as f:
+        with open(SAVE_BALANCE_IML, 'r') as f:
             dt = f.readline().split(';')[2]
             if dt[:-1] == datetime.datetime.now().strftime(format='%Y-%m-%d'):
                 f.seek(0)
@@ -32,20 +49,21 @@ def render_balance_iml():
                         'dt': _[2][:-1],
                     })
                 get_balance_from_file = True
-                print('From file')
+                print('get balance from file')
 
     except Exception as err:
         print(err)
 
     if not get_balance_from_file:
-        print('From API')
+        print('get balance from API')
         balance = iml_check_balance(None, IML_LOGIN, IML_PWD1)
-        with open('utils/balance.csv', 'w') as f:
+        with open(SAVE_BALANCE_IML, 'w') as f:
             for b in balance:
                 f.write(';'.join([b['sim_type'], b['iml_balance'], b['dt']]))
                 f.write('\n')
 
-    r = '''
+    title = 'Остатки сим-карт на складе IML'
+    body = '''
     <a href="/"><-- back</a><br>
     <table border="1">
         <tr>
@@ -55,7 +73,7 @@ def render_balance_iml():
     '''
 
     for b in balance:
-        r += '<tr><td>{}</td><td>{}</td></tr>'.format(b['sim_type'], b['iml_balance'])
+        body += '<tr><td>{}</td><td>{}</td></tr>'.format(b['sim_type'], b['iml_balance'])
 
-    r += '</table>'
-    return r
+    body += '</table>'
+    return render_page(title, body)
