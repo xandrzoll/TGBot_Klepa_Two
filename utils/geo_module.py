997cf3e0faf53bbe88ff27adb50ef8727a41e7a0
Alex Rudkovskiy <xandr.rudkovsky@gmail.com>
2020-02-17 22:23:06 +0300

    Add geo_module for check addresses

---
+++ utils/geo_module.py
@@ -0,0 +1,47 @@
+import requests
+# from ..config import YA_KEY
+
+
+BASE_URL = r'https://geocode-maps.yandex.ru/1.x/'
+YA_KEY = '9e7f87dc-438e-4ccc-bddc-dfc7566a92f0'
+
+
+
+def get_geo_coordinates(adr):
+    post_data = dict()
+    post_data['apikey'] = YA_KEY
+    post_data['format'] = 'json'
+    post_data['geocode'] = adr
+
+    try:
+        r = requests.get(BASE_URL, post_data)
+        pos = r.json()['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
+    except Exception as err:
+        pos = ''
+        print(err)
+    return pos
+
+
+def load_addresses_geo():
+    adrs = []
+    with open(r'D:\projects\TGBot_Klepa_Two\utils\AddressBook.csv', 'r', encoding='utf-8') as f:
+        for line in f.readlines()[1:]:
+            adr = line.split(';')
+            # adr[0] = int(adr[0])
+            if adr[2] == '""' or not adr[2] or adr[2] == '0':
+                geo = get_geo_coordinates(adr[1].replace('"', ''))
+                if geo:
+                    adr[2], adr[3] = geo.split()
+            adrs.append(adr)
+
+    with open(r'D:\projects\TGBot_Klepa_Two\utils\AddressBook.csv', 'w', encoding='utf-8') as f:
+        f.write('"indx";"full_address_text";"lat";"lon"\n')
+        for adr in adrs:
+            f.write(';'.join(adr))
+            f.write('\n')
+    return adrs
+
+
+if __name__ == '__main__':
+    adrs = load_addresses_geo()
+    print(adrs)
